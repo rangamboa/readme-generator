83e1fffa689ac810a875f3e1f8b204d0c26cca32
rangamboa <rangamboa@gmail.com>
2021-07-15 20:43:32 -0700

    refining markdown formatting

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 // Include packages needed for this application.
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generateREADME = (answers) => `
-# ${answers.projTitle}
+const licenseMIT = `
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+
+const licenseApache = `
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+  [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.`;
+
+const licenseGPL = `This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
+
+const generateREADME = (answers) =>
+`# ${answers.projTitle}
+${answers.projLicBadge}
 
 ## Table of Contents
+1. [Description](#description)
+2. [License](#license)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [Contribution](#contribution)
+5. [Testing](#testing)
+6. [Questions](#questions)
 
 ## Description
 
 ${answers.projDesc}
 
-## Installation Instructions
+## License
+
+This project is covered under the ${answers.projLic} license:
+
+${answers.projLicTxt}
+
+## Installation
 
 ${answers.projInstall}
 
-## Usage Information
+## Usage
 
 ${answers.projUsage}
 
-## Contribution Guidelines
+## Contribution
 
 ${answers.projCont}
 
@@ -27,100 +60,98 @@ ${answers.projCont}
 
 ${answers.projTest}
 
-## License
-
-This project is covered under the ${answers.projLic} license.
-
 ## Questions
 
 Have questions or need clarification? Please don't hesitate to reach out to me.
 
-* [GitHub Profile](https://github.com/${projGit}) 
+* [GitHub Profile](https://github.com/${answers.projGit}) 
 
-* [Email](mailto:${projEmail})
-
+* [Email](mailto:${answers.projEmail})
+---
+@2021
 `;
 
+const init = () => {
 
-inquirer
-    // Create an array of questions for user input to build README.md.
-                                                                            // const questions = [];
-    .prompt([
-    {
-        type: 'input',
-        name: 'projTitle',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'projDesc',
-        message: 'What is the project description?',
-    },
-    {
-        type: 'input',
-        name: 'projInstall',
-        message: 'What are the installation instructions?',
-    },
-    {
-        type: 'input',
-        name: 'projUsage',
-        message: 'Please enter any usage information for this project.',
-    },
-    {
-        type: 'input',
-        name: 'projCont',
-        message: 'Please enter any contribution guidelines for this project.',
-    },
-    {
-        type: 'input',
-        name: 'projTest',
-        message: 'Please enter any test instructions for this project.',
-    },
-    {
-        type: 'checkbox',
-        name: 'projLic',
-        message: 'Under which license is this project covered?',
-        choices: ['MIT', 'Apache', 'GPL'],
-    },
-    {
-        type: 'input',
-        name: 'projGit',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'projEmail',
-        message: 'What is your email address?',
-    },
-    ])
+    inquirer
+        // Create an array of questions for user input to build README.md.
 
-    .then((answers) => {
-        // Generate README.md and its sections, using input from user.
+        .prompt([
+        {
+            type: 'input',
+            name: 'projTitle',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'projDesc',
+            message: 'What is the project description?',
+        },
+        {
+            type: 'input',
+            name: 'projInstall',
+            message: 'What are the installation instructions?',
+        },
+        {
+            type: 'input',
+            name: 'projUsage',
+            message: 'Please enter any usage information for this project.',
+        },
+        {
+            type: 'input',
+            name: 'projCont',
+            message: 'Please enter any contribution guidelines for this project.',
+        },
+        {
+            type: 'input',
+            name: 'projTest',
+            message: 'Please enter any test instructions for this project.',
+        },
+        {
+            type: 'list',
+            name: 'projLic',
+            message: 'Under which license is this project covered?',
+            choices: ['MIT', 'Apache', 'GPL'],
+        },
+        {
+            type: 'input',
+            name: 'projGit',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'projEmail',
+            message: 'What is your email address?',
+        },
+        ])
 
-        const htmlPageContent = generateHTML(answers);
+        .then((answers) => {
 
-        fs.writeFile('README.md', readmeContent, (err) =>
-            err ? console.log(err) : console.log('Generated a README.md file.')
-        );
-    })
+            // console.log(answers);
 
-    .catch((error) => {
-    
-        if (error.isTtyError) {
-                                    // Prompt couldn't be rendered in the current environment
-        } else {
-                                    // Something else went wrong
-        }
-    });
+            // let answers.projLicTxt;
+            // let answers.projLicBadge;
 
+            if (answers.projLic == 'MIT') {
+                answers.projLicTxt = licenseMIT;
+                answers.projLicBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+            if (answers.projLic == 'Apache') {
+                answers.projLicTxt = licenseApache;
+                answers.projLicBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            }
 
+            if (answers.projLic == 'GPL') {
+                answers.projLicTxt = licenseGPL;
+                answers.projLicBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+            }
+          
+            // console.log(answers.projLicTxt);
 
-// TODO: Create a function to initialize app
-function init() {}
+            fs.writeFile('README.md', generateREADME(answers), (err) =>
+            err ? console.log(err) : console.log('Generated readme.md'));
+        })
+};
 
-
-// Function call to initialize app
 init();
