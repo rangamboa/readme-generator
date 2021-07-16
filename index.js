f3895fb20b0e64e9f197348b8377ecb778ad58a1
rangamboa <rangamboa@gmail.com>
2021-07-15 21:41:00 -0700

    working on modular scripts

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 // Include packages needed for this application.
 const fs = require('fs');
 const inquirer = require('inquirer');
-
-const licenseMIT = `
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
-
-const licenseApache = `
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-
-  [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.`;
-
-const licenseGPL = `This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
+const licenseData = require('./utils/generateMarkdown.js');
 
 const generateREADME = (answers) =>
 `# ${answers.projTitle}
@@ -111,7 +93,7 @@ const init = () => {
             type: 'list',
             name: 'projLic',
             message: 'Under which license is this project covered?',
-            choices: ['MIT', 'Apache', 'GPL'],
+            choices: ['MIT', 'Apache', 'GPL', 'none'],
         },
         {
             type: 'input',
@@ -127,27 +109,26 @@ const init = () => {
 
         .then((answers) => {
 
-            // console.log(answers);
+            // renderLicenseBadge(answers.projLic);
+            // renderLicenseLink(answers.projLic);
+            // renderLicenseSection(answers.projLic);
 
-            // let answers.projLicTxt;
-            // let answers.projLicBadge;
+            // if (answers.projLic == 'MIT') {
+            //     answers.projLicTxt = licenseMIT;
+            //     answers.projLicBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            // }
 
-            if (answers.projLic == 'MIT') {
-                answers.projLicTxt = licenseMIT;
-                answers.projLicBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-            }
+            // if (answers.projLic == 'Apache') {
+            //     answers.projLicTxt = licenseApache;
+            //     answers.projLicBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            // }
 
-            if (answers.projLic == 'Apache') {
-                answers.projLicTxt = licenseApache;
-                answers.projLicBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-            }
-
-            if (answers.projLic == 'GPL') {
-                answers.projLicTxt = licenseGPL;
-                answers.projLicBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
-            }
+            // if (answers.projLic == 'GPL') {
+            //     answers.projLicTxt = licenseGPL;
+            //     answers.projLicBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+            // }
           
-            // console.log(answers.projLicTxt);
+            // // console.log(answers.projLicTxt);
 
             fs.writeFile('README.md', generateREADME(answers), (err) =>
             err ? console.log(err) : console.log('Generated readme.md'));
